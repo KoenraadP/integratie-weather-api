72d7b2835cc09a7a381f873cfb252b5c1ca76afc
koenraad.pecceu <koenraad.pecceu@gmail.com>
2025-06-04 11:37:25 +0200

    0406251137

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // api key en api url in constanten opslaan
-const apiKey = "";
+const apiKey = "9f0b2498bc5c13f59ed35457a7418d68";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
 
 // html elementen binden
@@ -12,3 +12,24 @@ const tdHumidity = document.getElementById("humidity");
 const tdCurrentTemp = document.getElementById("current-temp");
 const tdMaxTemp = document.getElementById("max-temp");
 const tdMinTemp = document.getElementById("min-temp");
+
+// data ophalen voor gekozen stad wanneer we op de knop klikken
+btnSearch.addEventListener("click", () => {
+    // stadsnaam uit input halen
+    const city = inpSearch.value.trim();
+    // correcte endpoint maken
+    // zie voorbeeld op https://openweathermap.org/current
+    const endpoint = `${apiUrl}&appid=${apiKey}&q=${city}`;
+    // data ophalen met fetch
+    fetch(endpoint)
+        .then(response => {
+            if(response.ok) return response.json();
+            else throw new Error("Stad niet gevonden!");
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            alert(error.message);
+        })
+})
